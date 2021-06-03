cc8440f05960f6265ad579177825f2b9fd780e0c
Nandhamaddy007 <nandhamaddy007@gmail.com>
2021-06-03 23:08:39 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -38,13 +38,17 @@ export default function App() {
       //console.log(option.option);
       return (
         <li>
-          {option.select}
+          <a className="trigger right-caret">{option.select}</a>
           {construct(option.option)}
         </li>
       );
     } else {
       // console.log(option);
-      return <li>{option.select}</li>;
+      return (
+        <li>
+          <a>{option.select}</a>
+        </li>
+      );
     }
   };
   let construct = options => {
@@ -56,7 +60,7 @@ export default function App() {
     if (options.option && options.option != '') {
       return <ul>{data}</ul>;
     } else {
-      return <ul className="dropdown-menu">{data}</ul>;
+      return <ul className="dropdown-menu sub-menu">{data}</ul>;
     }
   };
 
@@ -111,62 +115,74 @@ export default function App() {
   // console.log(out);
   return (
     <div>
-      <div class="dropdown">
-        <a
-          href="#"
-          class="btn btn-primary dropdown-toggle"
-          data-toggle="dropdown"
-        >
-          Click Here
-          <span class="caret" />
-        </a>
-        <ul class="dropdown-menu">
-          <li>
-            <a class="trigger right-caret">Level 1</a>
-            <ul class="dropdown-menu sub-menu">
-              <li>
-                <a href="#">Level 2</a>
-              </li>
-              <li>
-                <a class="trigger right-caret">Level 2</a>
-                <ul class="dropdown-menu sub-menu">
-                  <li>
-                    <a href="#">Level 3</a>
-                  </li>
-                  <li>
-                    <a href="#">Level 3</a>
-                  </li>
-                  <li>
-                    <a class="trigger right-caret">Level 3</a>
-                    <ul class="dropdown-menu sub-menu">
-                      <li>
-                        <a href="#">Level 4</a>
-                      </li>
-                      <li>
-                        <a href="#">Level 4</a>
-                      </li>
-                      <li>
-                        <a href="#">Level 4</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Level 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">Level 1</a>
-          </li>
-          <li>
-            <a href="#">Level 1</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* {out} */}
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+              Submenus
+            </a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li className="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                Dynamic Dropdown
+                <span class="caret" />
+              </a>
+              {out}
+            </li>
+            <li class="dropdown">
+              <a href="#" class=" dropdown-toggle" data-toggle="dropdown">
+                Static Dropdown
+                <span class="caret" />
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="trigger right-caret">Level 1</a>
+                  <ul class="dropdown-menu sub-menu">
+                    <li>
+                      <a href="#">Level 2</a>
+                    </li>
+                    <li>
+                      <a class="trigger right-caret">Level 2</a>
+                      <ul class="dropdown-menu sub-menu">
+                        <li>
+                          <a href="#">Level 3</a>
+                        </li>
+                        <li>
+                          <a href="#">Level 3</a>
+                        </li>
+                        <li>
+                          <a class="trigger right-caret">Level 3</a>
+                          <ul class="dropdown-menu sub-menu">
+                            <li>
+                              <a href="#">Level 4</a>
+                            </li>
+                            <li>
+                              <a href="#">Level 4</a>
+                            </li>
+                            <li>
+                              <a href="#">Level 4</a>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#">Level 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Level 1</a>
+                </li>
+                <li>
+                  <a href="#">Level 1</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
