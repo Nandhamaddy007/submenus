fec91e45d2abc743ce0cc5d7aaa7954ab6a04714
Nandhamaddy007 <nandhamaddy007@gmail.com>
2021-06-03 21:50:05 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import './style.css';
+import React, { useEffect } from 'react';
+import $ from 'jquery';
+import './styles.css';
 let ls = [];
 let out = [];
 
 export default function App() {
+  useEffect(() => {
+    $(function() {
+      $('.dropdown-menu > li > a.trigger').on('click', function(e) {
+        var current = $(this).next();
+        var grandparent = $(this)
+          .parent()
+          .parent();
+        if ($(this).hasClass('left-caret') || $(this).hasClass('right-caret'))
+          $(this).toggleClass('right-caret left-caret');
+        grandparent
+          .find('.left-caret')
+          .not(this)
+          .toggleClass('right-caret left-caret');
+        grandparent
+          .find('.sub-menu:visible')
+          .not(current)
+          .hide();
+        current.toggle();
+        e.stopPropagation();
+      });
+      $('.dropdown-menu > li > a:not(.trigger)').on('click', function() {
+        var root = $(this).closest('.dropdown');
+        root.find('.left-caret').toggleClass('right-caret left-caret');
+        root.find('.sub-menu:visible').hide();
+      });
+    });
+  });
   let rend = option => {
     console.log(option);
     if (option.option && option.option != '') {
@@ -81,5 +109,64 @@ export default function App() {
   ];
   out = construct(options);
   // console.log(out);
-  return <div>{out}</div>;
+  return (
+    <div>
+      <div class="dropdown">
+        <a
+          href="#"
+          class="btn btn-primary dropdown-toggle"
+          data-toggle="dropdown"
+        >
+          Click Here
+          <span class="caret" />
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <a class="trigger right-caret">Level 1</a>
+            <ul class="dropdown-menu sub-menu">
+              <li>
+                <a href="#">Level 2</a>
+              </li>
+              <li>
+                <a class="trigger right-caret">Level 2</a>
+                <ul class="dropdown-menu sub-menu">
+                  <li>
+                    <a href="#">Level 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Level 3</a>
+                  </li>
+                  <li>
+                    <a class="trigger right-caret">Level 3</a>
+                    <ul class="dropdown-menu sub-menu">
+                      <li>
+                        <a href="#">Level 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Level 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Level 4</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Level 2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Level 1</a>
+          </li>
+          <li>
+            <a href="#">Level 1</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* {out} */}
+    </div>
+  );
 }
